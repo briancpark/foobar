2ce4eb49f99a51f6c96013f265de6dfead28d340
Brian Park <briancpark@berkeley.edu>
2020-12-22 02:30:29 -0500

    finished stage 3

---
+++ solution6.py
@@ -0,0 +1,17 @@
+def solution(l):
+    # Your code here
+    permutations = 0
+    combinations = [[] for _ in range(len(l))]
+
+    for i in range(1, len(l)):
+        for j in range(i):
+            if l[i] % l[j] == 0:
+                combinations[i].append(j)
+    
+    for i in range(2, len(l)):
+        for j in range(1, i):
+            if l[i] % l[j] == 0:
+                permutations += len(combinations[j])
+                
+    return permutations
+    
